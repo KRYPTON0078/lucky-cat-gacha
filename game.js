ba445addb04db96823910d89dbe8c3efd1941cc9
KRYPTON0078 <142139028+KRYPTON0078@users.noreply.github.com>
2026-09-05 15:36:56 +0800

    Update game.js (+1 line, total 20)

--- game.js
+++ game.js
@@ -17,3 +17,4 @@ const rewardsListEl = document.getElementById('rewards-list');
 const historyListEl = document.getElementById('history-list');
 const chancesCountEl = document.getElementById('chances-count');
 const gachaMachineEl = document.getElementById('gacha-machine');
+const resultDisplayEl = document.getElementById('result-display');
