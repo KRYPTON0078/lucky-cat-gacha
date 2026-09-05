ac4080cdb7934ab0ec4f2579d6a9858868e4d98d
KRYPTON0078 <142139028+KRYPTON0078@users.noreply.github.com>
2026-09-05 15:36:54 +0800

    Update game.js (+1 line, total 6)

--- game.js
+++ game.js
@@ -3,3 +3,4 @@ const REWARDS = [
   { name: 'Pink Lucky Cat', prob: 6.67, icon: '🐱', cssClass: 'pink' },
   { name: 'Blue Lucky Cat', prob: 13.33, icon: '🐱', cssClass: 'blue' },
   { name: 'Cat Sticker', prob: 26.67, icon: '🏷️', cssClass: 'sticker' },
+  { name: 'Thank You for Participating', prob: 50, icon: '💝', cssClass: 'thanks' },
